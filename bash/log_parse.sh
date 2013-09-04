f514b767be93be30ad3bb4f2a734daeb72d47f16
eldermyoshida <elder.yoshida@gmail.com>
2013-09-04 00:28:29 -0400

    Bash assignment regex correction

--- bash/log_parse.sh
+++ bash/log_parse.sh
@@ -2,10 +2,10 @@ echo "Hello World!"
 bunzip2 -f -k chronline.2013.08.27.bz2
 
 grep 'Started GET' chronline.2013.08.27 |
-    sed 's/.Started GET ".*" for \([0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\).*\/\1/' |
-        sort > addressess
+    sed 's/.*Started GET ".*" for \([0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\).*/\1/' |
+        sort > addresses
 
-ADDRESSESS=$(cat addresses | wc -l)
-DUKE = $(grep '152.3.43' addresses | wc -l)
+ADDRESSES=$(cat addresses | wc -l)
+DUKE=$(grep '152.3.43' addresses | wc -l)
 
-echo "Total $DUKE/$ADDRESSES"
+echo "Total Duke Addresses: $DUKE/$ADDRESSES"
